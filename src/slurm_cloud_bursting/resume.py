c74023a74fac70c6481ca58506f12287fd2e3de9
Francesco De Martino <fdm@amazon.com>
2020-05-21 19:19:37 +0200

    wip

--- src/slurm_cloud_bursting/resume.py
+++ src/slurm_cloud_bursting/resume.py
@@ -1,51 +1,35 @@
-#!/usr/local/pyenv/versions/slurm/bin/python
+#!/usr/local/pyenv/versions/node_virtualenv/bin/python
 
-import argparse
 import collections
 import logging
 import shlex
 import subprocess
+
+import argparse
 import boto3
 
-### Manual steps
-# 1. add to LaunchTemplate the subnet-id in the network interface
-# 2. add policy to run new intances
+REGION = "us-east-2"
+CLUSTER_NAME = "hit-integration-5"
+TAG_SPECIFICATIONS = [{"ResourceType": "instance", "Tags": [{"Key": "Name", "Value": "cloud-bursting-compute",}]}]
 
-REGION = "eu-west-1"
-LAUNCH_TEMPLATE = {
-    "LaunchTemplateId": "lt-06d29df441fc7fe83"
-}
-SUBNET_ID = "subnet-0f45a3009addd0e7f"
-TAG_SPECIFICATIONS = [
-    {
-        "ResourceType": "instance",
-        "Tags": [
-            {
-                "Key": "Name",
-                "Value": "cloud-bursting-compute",
-            }
-        ]
-    }
-]
-
-SCONTROL = '/opt/slurm/bin/scontrol'
-LOGFILE = '/home/slurm/slurm_resume.log'
+SCONTROL = "/opt/slurm/bin/scontrol"
+LOGFILE = "/home/slurm/slurm_resume.log"
 INSTANCES_BATCH_SIZE = 10
 
 # Set to True if the nodes aren't accessible by dns.
 UPDATE_NODE_ADDRS = True
 
-EC2Instance = collections.namedtuple(
-    "EC2Instance", ["id", "private_ip", "hostname"]
-)
+EC2Instance = collections.namedtuple("EC2Instance", ["id", "private_ip", "hostname"])
 
 
-def run_command(command, capture_output=True, log_error=True, log_output=True, env=None):
+def run_command(command, log_error=True, log_output=True, env=None):
     """Execute shell command."""
     if isinstance(command, str):
         command = shlex.split(command)
     logging.info("Executing command: " + " ".join(command))
-    result = subprocess.run(command, capture_output=capture_output, universal_newlines=True, encoding="utf-8", env=env)
+    result = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, encoding="utf-8", env=env
+    )
     try:
         result.check_returncode()
     except subprocess.CalledProcessError:
@@ -63,36 +47,55 @@ def run_command(command, capture_output=True, log_error=True, log_output=True, e
     return result
 
 
-def update_slurm_node_addrs(instances, slurm_nodes):
-    for instance, slurm_node in zip(instances, slurm_nodes):
+def update_slurm_node_addrs(instances):
+    for instance, slurm_node in instances:
         node_update_cmd = "{} update node={} nodeaddr={} nodehostname={}".format(
-            SCONTROL, slurm_node, instance.private_ip, instance.hostname)
+            SCONTROL, slurm_node, instance.private_ip, instance.hostname
+        )
         run_command(node_update_cmd)
         logging.info("Instance " + slurm_node + " is now configured")
 
 
 def _parse_ec2_instance(instance):
-    return EC2Instance(instance["InstanceId"],
-                       private_ip=instance["PrivateIpAddress"],
-                       hostname=instance["PrivateDnsName"].split(".")[0])
+    return EC2Instance(
+        instance["InstanceId"],
+        private_ip=instance["PrivateIpAddress"],
+        hostname=instance["PrivateDnsName"].split(".")[0],
+    )
 
 
 def add_instances(node_list):
-    ec2_client = boto3.client('ec2', region_name=REGION)
-    total = len(node_list)
+    ec2_client = boto3.client("ec2", region_name=REGION)
+
+    instances_to_launch = collections.defaultdict(lambda: collections.defaultdict(list))
+    for node in node_list:
+        queue_name = node.split("-")[0]
+        instance_type = node.split("-")[2]
+        instances_to_launch[queue_name][instance_type].append(node)
+
     launched_instances = []
-    while total > 0:
-        batch_count = min(total, INSTANCES_BATCH_SIZE)
-        result = ec2_client.run_instances(LaunchTemplate=LAUNCH_TEMPLATE,
-                                          MinCount=batch_count,
-                                          MaxCount=batch_count,
-                                          InstanceType="c5.xlarge",
-                                          TagSpecifications=TAG_SPECIFICATIONS)
-        launched_instances.extend([_parse_ec2_instance(instance) for instance in result["Instances"]])
-        total -= batch_count
+    for queue, queue_instances in instances_to_launch.items():
+        for instance_type, instances_list in queue_instances.items():
+            total = len(instances_list)
+            while total > 0:
+                batch_count = min(total, INSTANCES_BATCH_SIZE)
+                result = ec2_client.run_instances(
+                    LaunchTemplate={"LaunchTemplateName": CLUSTER_NAME + "-" + queue},
+                    MinCount=batch_count,
+                    MaxCount=batch_count,
+                    InstanceType=instance_type,
+                    TagSpecifications=TAG_SPECIFICATIONS,
+                )
+                launched_instances.extend(
+                    [
+                        (_parse_ec2_instance(ec2_instance), slurm_instance)
+                        for ec2_instance, slurm_instance in zip(result["Instances"], instances_list)
+                    ]
+                )
+                total -= batch_count
 
     if UPDATE_NODE_ADDRS:
-        update_slurm_node_addrs(launched_instances, node_list)
+        update_slurm_node_addrs(launched_instances)
 
 
 def main(arg_nodes):
@@ -109,20 +112,16 @@ def main(arg_nodes):
         if not len(retry_list):
             break
 
-        logging.debug("got {} nodes to retry ({})".
-                      format(len(retry_list), ",".join(retry_list)))
+        logging.debug("got {} nodes to retry ({})".format(len(retry_list), ",".join(retry_list)))
         node_list = list(retry_list)
         del retry_list[:]
 
     logging.debug("done adding instances")
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-    parser.add_argument('nodes', help='Nodes to burst')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter,)
+    parser.add_argument("nodes", help="Nodes to burst")
 
     args = parser.parse_args()
 
@@ -130,10 +129,7 @@ if __name__ == '__main__':
     for logger in logging.Logger.manager.loggerDict:
         logging.getLogger(logger).setLevel(logging.WARNING)
 
-    logging.basicConfig(
-        filename=LOGFILE,
-        format='%(asctime)s %(name)s %(levelname)s: %(message)s',
-        level=logging.DEBUG)
+    logging.basicConfig(filename=LOGFILE, format="%(asctime)s %(name)s %(levelname)s: %(message)s", level=logging.DEBUG)
 
     try:
         main(args.nodes)
